47596ebbd10b25751b89923c778d4a9ab99b1faa
Iaphetes <iaphetes@mailfence.com>
2023-04-15 13:08:13 +0200

    Allow only one instance running at any time

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,18 @@ mod config;
 mod egui_ui;
 use crate::apps::ApplicationManager;
 use crate::config::{load_config, Config};
+use single_instance::SingleInstance;
 fn main() {
-    // let mut config_path: PathBuf = home_dir().expect("No home directory ");
-    // config_path.push(".config/aphorme/config.toml");
-    let cfg: Config = load_config(None);
-    let application_manager: ApplicationManager =
-        ApplicationManager::new(cfg.app_cfg.unwrap_or_default(), cfg.gui_cfg.icon);
-    match egui_ui::launch_egui_ui(cfg.gui_cfg, application_manager) {
-        Ok(()) => {}
-        Err(error) => println!("{error:?}"),
-    };
+    let instance = SingleInstance::new("Aphorme").unwrap();
+    if instance.is_single() {
+        let cfg: Config = load_config(None);
+        let application_manager: ApplicationManager =
+            ApplicationManager::new(cfg.app_cfg.unwrap_or_default(), cfg.gui_cfg.icon);
+        match egui_ui::launch_egui_ui(cfg.gui_cfg, application_manager) {
+            Ok(()) => {}
+            Err(error) => println!("{error:?}"),
+        };
+    } else {
+        println!("another instance is already running");
+    }
 }
